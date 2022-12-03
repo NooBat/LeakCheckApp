9229181017d233a429da5e08eb2e35b13932385e
NooBat <khoi27012003@gmail.com>
2022-12-03 11:06:11 +0700

    commit by vscode

--- server/src/bin/main.sh
+++ server/src/bin/main.sh
@@ -5,11 +5,16 @@ cwd=$1
 cd $cwd
 
 echo "######################################################################"
-echo "#####                  Starting processes...                     #####"
-echo "######################################################################"
+echo "#####                   Starting processes...                    #####"
 
-echo "[1] Building source file..."
-g++ -g main.cpp ConcatStringTree.cpp -I . -o main
+echo "#####                [1] Building source file...                 #####"
 
-echo "[2] Running..."
-./main
+code=$(g++ -std=c++11 -g main.cpp ConcatStringTree.cpp -I . -o main)
+
+if [[ $code -eq 1 ]]
+then
+  echo "#####               Failed to build source file...               #####"
+else 
+  echo "#####                     [2] Running...                         #####"
+  ./main
+fi
